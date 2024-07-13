22a8b039847169908d17fd718c60eff9c8e5e480
Saurabh Gautam <saurabhgautam0101@gmail.com>
2024-07-13 19:28:27 +0530

    CRUD in mongoDB

--- Mongo/Mongo2.js
+++ Mongo/Mongo2.js
@@ -1,8 +1,26 @@
 const collection = require('./Connection/connect.js')
 
-const users = async ()=>{
+const ReadUsers = async ()=>{
     let data = await collection();
     data = await data.find({name : "saurabh"}).toArray();
     console.log(data)
 }
-users()
+// ReadUsers()
+
+const UpdateUsers = async ()=>{
+    let results = await collection();
+    let update = await results.insertOne({name : 'Rahul CHikna',developer : 'backend'})
+    results = await results.find().toArray();
+    console.log(results)
+    
+}
+// UpdateUsers()
+
+const DeleteUsers = async ()=>{
+    let data = await collection();
+    let deleteOne = await data.deleteOne({name : "Rahul CHikna"});
+    let deleteMany = await data.deleteMany({name : "Rahul"});
+    data = await data.find().toArray();
+    console.log(data)
+}
+// DeleteUsers()
